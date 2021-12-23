85f2a8ba805d542f01c2686697fded186a31ac22
abulaziz <abdulazizkomilov2001@gmail.com>
2021-12-23 09:45:28 +0500

    adds

--- 9_dars.py
+++ 9_dars.py
@@ -1,10 +1,10 @@
-# yosh = int(input('Yoshingiz nechida? '))
-# if yosh<=4:
-#     print('Sizga kirish bepul.')
-# elif yosh<=12:
-#     print('Sizga kirish 5000 so\'m')
-# else:
-#     print('Sizga kirish 10000 so\'m')
+yosh = int(input('Yoshingiz nechida? '))
+if yosh<=4:
+    print('Sizga kirish bepul.')
+elif yosh<=12:
+    print('Sizga kirish 5000 so\'m')
+else:
+    print('Sizga kirish 10000 so\'m')
 
 # yosh = int(input('Yoshingiz nechida? '))
 # if yosh<=4:
