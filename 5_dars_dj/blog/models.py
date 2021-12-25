dcbfb7a8ef4d0b2eb1aba893693cc86749c87200
abulaziz <abdulazizkomilov2001@gmail.com>
2021-12-25 09:59:03 +0500

    adds

--- 5_dars_dj/blog/models.py
+++ 5_dars_dj/blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
 
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+    )
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.pk)])
